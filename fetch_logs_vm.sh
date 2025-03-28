d5a05286dc0144f481fc66876bb995b4d3608903
Ed Chapman - Turing <edchapmanelc@gmail.com>
2025-03-28 15:40:15 +0000

    ppo agent experiment

--- fetch_logs_vm.sh
+++ fetch_logs_vm.sh
@@ -1,9 +1,20 @@
 date=$(date '+%Y-%m-%d_%H:%M:%S')
 
-scp blue@$HILBERT_VM:/var/log/telegraf/telegraf.json ~/projects/got/experiments/$1/logs/hilbert/$date.json
+HILBERT_IP=172.0.0.3
+KLEENE_IP=172.0.0.2
+POST_IP=172.0.0.4
 
-scp blue@$HILBERT_VM:/home/blue/blue_logs/* ~/projects/got/experiments/$1/logs/hilbert
+# Fetch logs from Hilbert
+scp blue@$HILBERT_IP:/var/log/telegraf/telegraf.json ~/projects/got/experiments/$1/logs/hilbert/$date.json
 
-scp blue@$KLEENE_VM:/var/log/telegraf/telegraf.json ~/projects/got/experiments/$1/logs/kleene/$date.json
+scp blue@$HILBERT_IP:/home/blue/blue_logs/* ~/projects/got/experiments/$1/logs/hilbert
 
-scp blue@$KLEENE_VM:/home/blue/got_logs/* ~/projects/got/experiments/$1/logs/kleene
+# Fetch logs from Kleene
+scp blue@$KLEENE_IP:/var/log/telegraf/telegraf.json ~/projects/got/experiments/$1/logs/kleene/$date.json
+
+scp blue@$KLEENE_IP:/home/blue/got_logs/* ~/projects/got/experiments/$1/logs/kleene
+
+# Fetch logs from Post
+scp red@$POST_IP:/var/log/telegraf/telegraf.json ~/projects/got/experiments/$1/logs/post/$date.json
+
+scp red@$POST_IP:/home/red/got_logs/* ~/projects/got/experiments/$1/logs/post
